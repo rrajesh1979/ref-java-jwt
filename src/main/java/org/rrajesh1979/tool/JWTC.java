de8e20f1c3b47d67c1a7abfc54bc9e3e1570e257
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-09 21:06:05 -0500

    :memo: License header updated in all files

--- src/main/java/org/rrajesh1979/tool/JWTC.java
+++ src/main/java/org/rrajesh1979/tool/JWTC.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) 2022 Rajesh Rajagopalan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.rrajesh1979.tool;
 
 import java.util.concurrent.Callable;
@@ -5,7 +21,6 @@ import java.util.concurrent.Callable;
 import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Help;
 import picocli.CommandLine.Help.Ansi.Style;
 import picocli.CommandLine.Help.ColorScheme;
 import picocli.CommandLine.HelpCommand;
